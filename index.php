583653b7718d369cbea83936e96817c5b2481ce6
Haris-aja <43005207+dwiharis13@users.noreply.github.com>
2020-03-09 22:48:31 +0700

    Update index.php
    update login

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'koneksi.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Login V1</title>
+  <title>Halaman Login</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->  
